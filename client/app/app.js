6b284b92a37247c33023523ffc6d877c6af4693f
Aaron Te <aaronjte@gmail.com>
2015-07-18 16:31:32 -0400

    Fixing routing problems.

--- client/app/app.js
+++ client/app/app.js
@@ -1,15 +1,42 @@
 'use strict';
 
 angular.module('boltApp', [
-  'ngCookies',
-  'ngResource',
-  'ngSanitize',
-  'ui.router',
-  'ui.bootstrap'
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ui.router',
+    'ui.bootstrap'
 ])
-  .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
-    $urlRouterProvider
-      .otherwise('/');
+    .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+        $urlRouterProvider
+            .otherwise('/');
 
-    $locationProvider.html5Mode(true);
-  });
+        $locationProvider.html5Mode(true);
+
+        $stateProvider
+            .state('dashboard', {
+                url: '/',
+                templateUrl: 'app/main/main.html',
+                controller: 'MainCtrl'
+            })
+            .state('group-creation', {
+                url: '/create/group',
+                templateUrl: '',
+                controller: ''
+            })
+            .state('group-view', {
+                url: '/groups/:groupId',
+                templateUrl: '',
+                controller: ''
+            })
+            .state('event-creation', {
+                url: '/groups/:groupId/create/event',
+                templateUrl: '',
+                controller: ''
+            })
+            .state('event-view', {
+                url: 'groups/:groupId/events/:eventId',
+                templateUrl: '',
+                controller: ''
+            })
+    });
