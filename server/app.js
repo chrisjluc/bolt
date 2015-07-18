7eee3e415b629f43ea6bfecc059ef6af84c17a99
Christopher Luc <chris.luc.dev@gmail.com>
2015-07-18 17:03:54 -0400

    Add paypal code

--- server/app.js
+++ server/app.js
@@ -10,6 +10,20 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var express = require('express');
 var mongoose = require('mongoose');
 var config = require('./config/environment');
+var paypal = require('paypal-rest-sdk');
+
+// Set up paypal
+paypal.configure({
+    'mode': 'sandbox',
+    'client_id': 'AUXGapTnFrkF7NSH6-sopE_DXA7ktDd3sgRUmHR9IWxoCRajy7fLzCPqAKeGA1wdAWa0LuXEQadEj1Eq',
+    'client_secret': 'EGLv26LPeyvJRYTru4Qm8EuxPB6qK3XrzZ3fbvNa85EqTStQ6GlZIwMlQYYP60utcOoNOgnDKCYGlO4F',
+    'openid_redirect_uri': 'http://localhost:9000/auth'
+});
+
+var loginUrl = paypal.openIdConnect.authorizeUrl(
+        {'scope': 'openid'}
+);
+console.log(loginUrl);
 
 // Connect to database
 mongoose.connect(config.mongo.uri, config.mongo.options);
