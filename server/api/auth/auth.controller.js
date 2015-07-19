c06ec4fc410afcb5e5ca18580510e1db835d9178
Aaron Te <aaronjte@gmail.com>
2015-07-19 03:28:58 -0400

    Adding back removed function from merge conflict.

--- server/api/auth/auth.controller.js
+++ server/api/auth/auth.controller.js
@@ -6,10 +6,10 @@ var jwt = require('jwt-simple');
 var config = require('../../config/environment');
 
 var authController = {
-	authenticateUser: authenticateUser,
-	getPayPalUser: getPayPalUser,
-	ensureAuthenticated: ensureAuthenticated,
-	getBrainTreeClientToken: getBrainTreeClientToken,
+    authenticateUser: authenticateUser,
+    getPayPalUser: getPayPalUser,
+    ensureAuthenticated: ensureAuthenticated,
+    getBrainTreeClientToken: getBrainTreeClientToken,
     getUser: getUser,
     parseJoinToken: parseJoinToken
 };
@@ -17,124 +17,128 @@ var authController = {
 module.exports = authController;
 
 function getBrainTreeClientToken(req, res) {
-	gateway.clientToken.generate({}, function (err, response) {
-		res.send(response.clientToken);
-	});
+    gateway.clientToken.generate({}, function (err, response) {
+        res.send(response.clientToken);
+    });
 }
 
 function authenticateUser(req, res, next) {
-	var email = req.body.email;
-	var password = req.body.password;
+    var email = req.body.email;
+    var password = req.body.password;
 
-	var query = {
-		email: email
-	};
+    var query = {
+        email: email
+    };
 
-	User.
-		findOne(query, function (err, user) {
-			if (err) {
-				err = new Error('Error with authenticating user');
-				return next(err);
-			} else if (!_.isEqual(user.password, password)) {
-				return res.status(403).send('Wrong password');
-			} else {
-				return res.status(200).send({
-					user: user,
-					token: createToken(user)
-				});
-			}
-		});
+    User.
+        findOne(query, function (err, user) {
+            if (err) {
+                err = new Error('Error with authenticating user');
+                return next(err);
+            } else if (!_.isEqual(user.password, password)) {
+                return res.status(403).send('Wrong password');
+            } else {
+                return res.status(200).send({
+                    user: user,
+                    token: createToken(user)
+                });
+            }
+        });
 }
 
 function getPayPalUser(req, res) {
-	var code = req.params.code;
-	paypal.openIdConnect.tokeninfo.create(code, function (err, tokenInfo) {
-		if (err) {
-			return res.status(400).send(err);
-		}
+    var code = req.params.code;
+    paypal.openIdConnect.tokeninfo.create(code, function (err, tokenInfo) {
+        if (err) {
+            return res.status(400).send(err);
+        }
 
-		console.log(tokenInfo);
-		paypal.openIdConnect.userinfo.get(tokenInfo.access_token, function (err, userInfo) {
+        console.log(tokenInfo);
+        paypal.openIdConnect.userinfo.get(tokenInfo.access_token, function (err, userInfo) {
 
-			if (err) return res.status(400).send(err);
+            if (err) return res.status(400).send(err);
 
-			console.log(userInfo);
+            console.log(userInfo);
 
-			User.findOne({user_id: userInfo.user_id}, function (err, user) {
+            User.findOne({user_id: userInfo.user_id}, function (err, user) {
 
-				if (err) return res.status(400).send(err);
+                if (err) return res.status(400).send(err);
 
-				if (user) {
-					User.findByIdAndUpdate({_id: user._id}, {
-						access_token: tokenInfo.access_token,
-						refresh_token: tokenInfo.refresh_token,
-						token_expiry_date: Date.now() + 1000 * tokenInfo.expires_in
-					}, function (err) {
-						if (err) return res.status(400).send(err);
-						console.log('Updated existing user');
-						sendUser(user);
-					});
+                if (user) {
+                    User.findByIdAndUpdate({_id: user._id}, {
+                        access_token: tokenInfo.access_token,
+                        refresh_token: tokenInfo.refresh_token,
+                        token_expiry_date: Date.now() + 1000 * tokenInfo.expires_in
+                    }, function (err) {
+                        if (err) return res.status(400).send(err);
+                        console.log('Updated existing user');
+                        sendUser(user);
+                    });
 
-				} else {
-					var newUser = User({
-						first_name: userInfo.given_name,
-						last_name: userInfo.family_name,
-						email: userInfo.email,
-						user_id: userInfo.user_id,
-						access_token: tokenInfo.access_token,
-						refresh_token: tokenInfo.refresh_token,
-						token_expiry_date: Date.now() + 1000 * tokenInfo.expires_in
-					});
-					newUser.save(function (err, user) {
-						if (err) return res.status(400).send(err);
-						console.log('Created new user');
-						sendUser(user);
-					});
+                } else {
+                    var newUser = User({
+                        first_name: userInfo.given_name,
+                        last_name: userInfo.family_name,
+                        email: userInfo.email,
+                        user_id: userInfo.user_id,
+                        access_token: tokenInfo.access_token,
+                        refresh_token: tokenInfo.refresh_token,
+                        token_expiry_date: Date.now() + 1000 * tokenInfo.expires_in
+                    });
+                    newUser.save(function (err, user) {
+                        if (err) return res.status(400).send(err);
+                        console.log('Created new user');
+                        sendUser(user);
+                    });
 
-				}
-			});
-		});
-	});
+                }
+            });
+        });
+    });
 
-	function sendUser(user) {
-		var userData = {
-			user: user,
-			token: createToken(user)
-		};
-		res.status(200).send(userData);
-	}
+    function sendUser(user) {
+        var userData = {
+            user: user,
+            token: createToken(user)
+        };
+        res.status(200).send(userData);
+    }
 }
 
 function ensureAuthenticated(req, res, next) {
-	if (!req.headers.authorization) {
-		return res.status(401).send({message: 'Please make sure your request has an Authorization header'});
-	}
+    if (!req.headers.authorization) {
+        return res.status(401).send({message: 'Please make sure your request has an Authorization header'});
+    }
 
-	var token = req.headers.authorization.split(' ')[1];
-	var payload = jwt.decode(token, config.TOKEN_SECRET);
+    var token = req.headers.authorization.split(' ')[1];
+    var payload = jwt.decode(token, config.TOKEN_SECRET);
 
-	if (payload.exp <= moment().unix()) {
-		return res.status(401).send({message: 'Token has expired'});
-	}
+    if (payload.exp <= moment().unix()) {
+        return res.status(401).send({message: 'Token has expired'});
+    }
 
-	req.headers.userId = payload.sub;
-	User.findById(req.headers.userId, function (err, user) {
-		if (err) {
-			res.status(500).send({message: err});
-			return;
-		}
-		req.headers.user = user;
-		next();
-	});
+    req.headers.userId = payload.sub;
+    User.findById(req.headers.userId, function (err, user) {
+        if (err) {
+            res.status(500).send({message: err});
+            return;
+        }
+        req.headers.user = user;
+        next();
+    });
 }
 
 function createToken(user) {
-	var payload = {
-		sub: user._id,
-		iat: moment().unix(),
-		exp: moment().add(14, 'days').unix()
-	};
-	return jwt.encode(payload, config.TOKEN_SECRET);
+    var payload = {
+        sub: user._id,
+        iat: moment().unix(),
+        exp: moment().add(14, 'days').unix()
+    };
+    return jwt.encode(payload, config.TOKEN_SECRET);
+}
+
+function getUser(req, res) {
+    res.status(200).send(req.headers.user);
 }
 
 function parseJoinToken(req, res, next) {
